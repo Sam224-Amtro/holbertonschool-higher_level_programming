db484823ab88e2a0520d2a60ab163726a1147ee1
Samamtro <fofanasanoussy545@gmail.com>
2025-09-03 01:21:08 +0200

    the file to last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-import random # On importe le module random pour générer un nombre aléatoire
-number = random.randint(-10000, 10000) # Génère un nombre aléatoire entre -10000 et 10000
+import random
+number = random.randint(-10000, 10000)
 
 lastdigit = number % -10 if number < 0 else number % 10
-# Si le nombre est négatif → on prend number % -10 (donne un dernier chiffre négatif)
-# Sinon → on prend number % 10 (dernier chiffre positif)
+str = "Last digit of"
 
-str = "Last digit of" # Stocke le début du message à afficher
-
-if lastdigit > 5: # Si le dernier chiffre est supérieur à 5
+if lastdigit > 5:
     print(f"{str} {number} is {lastdigit} and is greater than 5")
-elif lastdigit == 0: # Si le dernier chiffre est égal à 0
+elif lastdigit == 0:
     print(f"{str} {number} is {lastdigit} and is 0")
-else: # Sinon (dernier chiffre < 6 et ≠ 0)
+else:
     print(f"{str} {number} is {lastdigit} and is less than 6 and not 0")
