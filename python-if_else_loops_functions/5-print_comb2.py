6cd6985435d46976849fb026d1c73b517e9b2168
Samamtro <fofanasanoussy545@gmail.com>
2025-09-03 11:25:06 +0200

    the file to print_comb2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-# On démarre une boucle qui parcourt les nombres de 0 à 99
-for i in range(100):
-    # Si le nombre n’est pas encore 99 (c’est-à-dire pas le dernier)
-    if i < 99:
-        # On affiche le nombre sur deux chiffres, suivi de ", "
-        # end="" permet de rester sur la même ligne
-        print(f"{i:02d}, ", end="")
+for k in range(100):
+    if k < 99:
+        print(f"{k:02d}, ", end="")
     else:
-        # Si c’est le dernier nombre (99), on l’affiche
-        # mais sans ajouter de virgule à la fin
-        print(f"{i:02d}")
+        print(f"{k:02d}")
