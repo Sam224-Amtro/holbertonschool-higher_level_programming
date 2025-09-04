3ec5bd146a85cf58f205412f3d2d9f01c40b25ae
Samamtro <fofanasanoussy545@gmail.com>
2025-09-04 23:07:10 +0200

    the file to nfinite_add

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    count = len(sys.argv)
+    add = 0
+
+    for k in range(1, count):
+        add += int(sys.argv[k])
+
+        print("{}".format(add))
