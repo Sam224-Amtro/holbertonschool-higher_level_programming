f99a74e74649f1a0cb5c1fdf10b077bbc470c420
Samamtro <fofanasanoussy545@gmail.com>
2025-09-08 13:19:24 +0200

    this is my file to element_at

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def element_at(my_list, idex):
-    if id < 0 or idex >= len(my_list):
+def element_at(my_list, idx):
+    if id < 0 or idx >= len(my_list):
         return None
-    return my_list[idex]
+    return my_list[idx]
