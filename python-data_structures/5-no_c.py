894423faf6ba0d1b6a999dd2881364634a2b3848
Samamtro <fofanasanoussy545@gmail.com>
2025-09-08 15:03:02 +0200

    the file to no_c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
+    # Créez une nouvelle chaîne en itérant à travers la chaîne d'origine
+    # et incluant uniquement des personnages qui ne sont pas «C» ou «C»
     new_string = ""
     for alphabet in my_string:
         if alphabet != 'c' and alphabet != 'C':
