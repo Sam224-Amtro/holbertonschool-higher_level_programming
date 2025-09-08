1d1f6d098e0e8ccbb582af699df839a317f7b388
Samamtro <fofanasanoussy545@gmail.com>
2025-09-08 16:23:48 +0200

    the file to add_tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = tuple_a + (0, 0)
-    tuple_b = tuple_b + (0, 0)
 
-    result = (tuple_a[0] + tuple_b[0], tuple[1] + tuple_b[1])
-    return result
+    # On complète les tuples pour avoir au moins 2 valeurs
+    a = (tuple_a + (0, 0))[:2]
+    b = (tuple_b + (0, 0))[:2]
+
+    return (a[0] + b[0], a[1] + b[1])
