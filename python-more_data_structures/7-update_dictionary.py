92e65c0297840171a1fe674475bcd89a589878ae
Samamtro <fofanasanoussy545@gmail.com>
2025-09-09 16:30:38 +0200

    the file to update_dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    # ajoute ou met à jour la clé avec la nouvelle valeur
+    a_dictionary[key] = value
+    return a_dictionary
