48c1be1217a25ff3b3f6a05a751fb1af034764e3
Samamtro <fofanasanoussy545@gmail.com>
2025-09-09 15:01:44 +0200

    the file to square_matrix_simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = []  # nouvelle matrice vide qui contiendra le résultat
+    new_matrix = [] # nouvelle matrice vide qui contiendra le résultat
 
-    for ligne in matrix:  # on parcourt chaque ligne de la matrice originale
-        new_ligne = []  # on crée une nouvelle ligne vide
-        for x in ligne:  # on parcourt chaque élément de la ligne
-            new_ligne.append(x ** 2)  # on ajoute le carré de l’élément
-        new_matrix.append(new_ligne)  # on ajoute la nouvelle ligne à la matrice
+    for ligne in matrix: # on parcourt chaque ligne de la matrice originale
+        new_ligne = [] # on crée une nouvelle ligne vide
+        for x in ligne: # on parcourt chaque élément de la ligne
+            new_ligne.append(x ** 2) # on ajoute le carré de l’élément
+        new_matrix.append(new_ligne) # on ajoute la nouvelle ligne à la matrice
 
-    return new_matrix  # on retourne la nouvelle matrice sans modifier l’originale
+    return new_matrix # on retourne la nouvelle matrice sans modifier l’originale
