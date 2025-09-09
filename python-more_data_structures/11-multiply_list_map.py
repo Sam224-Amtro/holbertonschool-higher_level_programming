1b8b7c7a03b9069cb512481e9e1a81a2ccb00d6b
Samamtro <fofanasanoussy545@gmail.com>
2025-09-09 17:36:41 +0200

    the file to multiply_list_map

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -2,4 +2,4 @@
 
 def multiply_list_map(my_list=[], number=0):
     # multiplie chaque élément de la liste par 'number'
-    return list(map(lambda x: x * number, my_list))
+    return (list(map(lambda x: x * number, my_list)))
