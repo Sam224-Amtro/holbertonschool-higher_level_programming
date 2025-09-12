01a9c853cd369f00dc83c8a24a828fac2c6e33e8
Samamtro <fofanasanoussy545@gmail.com>
2025-09-12 02:40:09 +0200

    this is the file to safe_print_list and README

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """Affiche au maximum x éléments d'une liste et retourne combien ont été affichés."""
+
+    # Compteur pour savoir combien d’éléments ont vraiment été affichés
+    count = 0
+
+    # On parcourt de 0 jusqu’à x-1
+    for i in range(x):
+        # Vérifie si l’index existe dans la liste (évite une erreur IndexError)
+        if i < len(my_list):
+            print(my_list[i], end="")  # affiche l’élément sans saut de ligne
+            count += 1                 # on incrémente le compteur
+        else:
+            break  # si on dépasse la taille de la liste, on arrête la boucle
+
+    print()  # saut de ligne final pour que l’affichage soit propre
+
+    # Retourne le nombre d’éléments effectivement affichés
+    return count
