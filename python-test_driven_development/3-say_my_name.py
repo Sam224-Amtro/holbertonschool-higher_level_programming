cb0ad915df344410238ff84b8038b390bc707db0
Samamtro <fofanasanoussy545@gmail.com>
2025-09-14 15:28:44 +0200

    the file to say_my_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-Prints the full name in the format: My name is <first name> <last name>.
+Module that prints a full name in the format:
+My name is <first name> <last name>.
 """
 
 def say_my_name(first_name, last_name=""):
     """
-    Prints the full name in the format: My name is <first name> <last name>.
+    Prints the full name.
 
     Args:
-        first_name (str): The first name.
-        last_name (str): The last name (optional).
+        first_name (str): the first name
+        last_name (str): the last name (optional)
 
     Raises:
-        TypeError: If first_name or last_name is not a string.
+        TypeError: if first_name or last_name is not a string
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    # Pas de strip() -> garde l’espace final si last_name est vide
-    print(f"My name is {first_name} {last_name}")
+    if last_name:
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
