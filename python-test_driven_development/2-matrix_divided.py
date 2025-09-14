cb8234278fa92f2950ffcd80a47556b3766b6067
Samamtro <fofanasanoussy545@gmail.com>
2025-09-14 02:47:07 +0200

    the file to matrix_divided

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
+"""
+Module Matrix divided
+
+Functions:
+    matrix_divided(matrix, div)
+"""
+
+
 def matrix_divided(matrix, div):
-    """Divides all elements of a matrix by div, rounded to 2 decimals."""
-
-    # Vérifier que matrix est une liste de listes
-    if not isinstance(matrix, list) or not matrix:
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-            )
-
-    # Vérifier que tous les éléments sont des nombres
-    for row in matrix:
-        if not all(isinstance(x, (int, float)) for x in row):
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats"
-                )
-
-    # Vérifier que toutes les lignes ont la même taille
-    if len(set(len(row) for row in matrix)) > 1:
-        raise TypeError("Each row of the matrix must have the same size")
-
-    # Vérifier div
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
+    """
+    Divide all elements of a matrix.
+    matrix must be a list of lists of integers or floats.
+    Each row of the matrix must have the same size.
+    div must be a number (integer or float) and can't be equal to 0.
+    Returns a new matrix, else raises TypeError or ZeroDivisionError.
+    """
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    elif not isinstance(div, (int, float)) or div == float("inf"):
+        raise TypeError("div must be a number")
 
-    # Créer une nouvelle matrice avec les valeurs divisées
-    new_matrix = [[round(x / div, 2) for x in row] for row in matrix]
-    return new_matrix
+    for ligne in matrix:
+        if len(matrix[0]) != len(ligne):
+            raise TypeError(
+                "Each row of the matrix must have the same size"
+            )
+
+    try:
+        new_mat = []
+        for ligne in matrix:
+            new_mat.append(list(map(lambda x: round(x / div, 2), ligne)))
+        return new_mat
+    except (TypeError, ValueError) as e:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
