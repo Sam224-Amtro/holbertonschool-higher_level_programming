17e7b6aea7e46252ded544f7507cf3ee37b44e56
Samamtro <fofanasanoussy545@gmail.com>
2025-09-14 03:25:39 +0200

    the file to say_my_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,18 +5,12 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Args:
-        first_name (str): string first name
-        last_name (str, optional): last name. Defaults to "".
-
-    Raises:
-        TypeError: Si le first_name n'est pas une string
-        TypeError: Si le last_name n'est pas une string
-    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print(f"My name is {first_name} {last_name}".strip())
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name} ")
