ad17d7e3f9afdde0ed99ac3599d19e77e6fa2847
Samamtro <fofanasanoussy545@gmail.com>
2025-09-15 11:28:20 +0200

    the file to square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Module définissant une classe Square encore vide."""
+"""Defines a class Square."""
 
 class Square:
-    """
-    Classe représentant un carré, sans attributs ni méthodes pour l'instant.
-    """
+    """Represent a square."""
     pass
