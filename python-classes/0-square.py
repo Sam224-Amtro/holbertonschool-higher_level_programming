3c87edb1a78c17f76fbbde1d7e4eae2a53e906e1
Samamtro <fofanasanoussy545@gmail.com>
2025-09-15 11:08:41 +0200

    This is the file to README and Square

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Définition d'une classe appelée Square (Carré)
+class Square:
+    pass  # 'pass' est une instruction vide : elle permet de créer une classe
+          # sans contenu pour l'instant (classe "vide" servant de squelette)
+
