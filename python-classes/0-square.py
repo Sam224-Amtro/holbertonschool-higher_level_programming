0970ad677b6cda6f11b9b7c7bb7ec24e6cc795a4
Samamtro <fofanasanoussy545@gmail.com>
2025-09-15 11:10:17 +0200

    the file to Square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,6 +2,7 @@
 
 # Définition d'une classe appelée Square (Carré)
 class Square:
-    pass  # 'pass' est une instruction vide : elle permet de créer une classe
-          # sans contenu pour l'instant (classe "vide" servant de squelette)
 
+    # 'pass' est une instruction vide : elle permet de créer une classe
+    # sans contenu pour l'instant (classe "vide" servant de squelette)
+    pass
