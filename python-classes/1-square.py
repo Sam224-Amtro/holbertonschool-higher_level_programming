baf69f4afae050655ce8df5cc5baa8e63c2e1946
Samamtro <fofanasanoussy545@gmail.com>
2025-09-15 12:13:50 +0200

    this is my file to 1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    # Définition d'une classe nommée "Square" (Carré)
+
+    def __int__(self, size):
+        self.__size = size
+        # Ici, l'intention est probablement de créer un constructeur
+        # qui initialise la taille du carré avec l'attribut privé __size.
+
