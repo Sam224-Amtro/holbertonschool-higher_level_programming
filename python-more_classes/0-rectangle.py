2a16e050070b1c6b11d64eff2b9e36822bd43c31
Samamtro <fofanasanoussy545@gmail.com>
2025-09-16 15:44:11 +0200

    this is the file to my README and 0-rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 0-rectangle
+defines empty class
+"""
+
+
+class rectangle:
+    """rectangle"""
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+Welcome to the projet python-more_classes
