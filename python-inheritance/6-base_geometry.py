c5af29c37238ece7a86264748784b997ab4d4b4f
Samamtro <fofanasanoussy545@gmail.com>
2025-09-22 15:45:40 +0200

    the file to base_geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+
+class BaseGeometry:
+    """A class représentant des opérations géométriques de base."""
+
+    def area(self):
+        """Raises une exception indiquant que la méthode
+        n'est pas implémentée."""
+        raise Exception("area() n'est pas implémenté")
