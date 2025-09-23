faffd41822b9c1afa03ac8731b86aebf9fe04abd
Samamtro <fofanasanoussy545@gmail.com>
2025-09-23 10:32:34 +0200

    the file to base_geometry

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,24 +6,24 @@ class BaseGeometry:
     """A class for basic geometric operations."""
 
     def area(self):
-        """Raises an exception indicating that the method is not
-        implemented."""
-        raise Exception("area() is not implemented")
+        """Raises une exception indiquant que la méthode n'est pas
+        implémenté."""
+        raise Exception("area() n'est pas implémenté")
 
     def integer_validator(self, name, value):
         """
-        Validates that the value is an integer and greater than 0.
+        Valide que la valeur est un entier et supérieur à 0.
 
         Args:
-            name (str): The name of the value (used in the exception
-            message.)
-            value (int): The value to validate.
+            name (str): Le nom de la valeur (utilisé dans l'exception
+        message.)
+            value (int): La valeur à valider.
 
         Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
+            TypeError: Si la valeur n'est pas un entier.
+            ValueError: Si la valeur est inférieure ou égale à 0.
         """
         if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError(f"{name} Doit être un entier")
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError(f"{name} doit être supérieur à 0")
