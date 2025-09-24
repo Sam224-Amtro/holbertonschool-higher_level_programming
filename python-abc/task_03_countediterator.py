75e50d4fc1001a92a7aa655bd4d7ca4f790fa44b
Samamtro <fofanasanoussy545@gmail.com>
2025-09-24 18:02:54 +0200

    the file to task_03_countediterator

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,31 +1,24 @@
 #!/usr/bin/env python3
-# Cette ligne spéciale (shebang) indique quel interpréteur utiliser.
-# Ici, on dit au système d’exécuter le script avec Python 3.
-# (Utile surtout sous Linux/Mac, pas nécessaire sous Windows.)
-
+# Shebang : utile sous Linux/Mac pour lancer directement le script avec Python 3
 
 class CountedIterator:
-    # Définition d'une classe qui va compter combien d'éléments on a parcourus
+    # Classe qui permet d’itérer en comptant combien d’éléments on parcourt
 
     def __init__(self, iterable):
-        # Le constructeur prend en paramètre un objet itérable
-        # (ex: liste, tuple, etc.)
-
-        # On transforme l'itérable en véritable itérateur
+        # On transforme l’itérable en itérateur
         self.iterator = iter(iterable)
         self.count = 0  # Compteur initialisé à 0
 
-        def __iter__(self):
-            # Cette méthode rend l'objet "itérable".
-            # Elle doit retourner un itérateur (ici : l'objet lui-même).
-            return self
+    def __iter__(self):
+        # L’objet est lui-même un itérateur
+        return self
 
-        def __next__(self):
-            # Cette méthode définit le comportement à chaque "next()"
-            item = next(self.iterator)  # Récupère le prochain élément
-            self.count += 1  # Incrémente le compteur
-            return item  # Retourne l'élément
+    def __next__(self):
+        # Retourne l’élément suivant et incrémente le compteur
+        item = next(self.iterator)  # Peut lever StopIteration quand fini
+        self.count += 1
+        return item
 
-        def get_count(self):
-            # Méthode pour récupérer la valeur du compteur
-            return self.count
+    def get_count(self):
+        # Retourne combien d’éléments ont été parcourus
+        return self.count
