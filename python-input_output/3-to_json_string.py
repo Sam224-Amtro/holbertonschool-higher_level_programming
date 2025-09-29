75c9e2a53fbe7a31e349411f8d5e808f2ac59b64
Samamtro <fofanasanoussy545@gmail.com>
2025-09-29 12:07:13 +0200

    the file to 3-to_json_string.

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,7 +2,8 @@
 """C'est pour JSON String"""
 
 
-# Importe le module 'json' qui permet de travailler avec le format JSON en Python.
+# Importe le module 'json' qui permet de travailler avec
+# le format JSON en Python.
 import json
 
 def to_json_string(my_obj):
