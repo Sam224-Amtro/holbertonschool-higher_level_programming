793743eecf5db839dfab99eb0a927634c7208540
Samamtro <fofanasanoussy545@gmail.com>
2025-09-29 13:55:13 +0200

    the file to 5-save_to_json_file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -2,15 +2,13 @@
 """Module pour JSON File"""
 import json
 
-
 def save_to_json_file(my_obj, filename):
     """
     Écrit un objet dans un fichier texte en utilisant sa représentation JSON.
 
-    Args :
-    my_obj : L'objet à sérialiser et à écrire dans le fichier.
-    filename : Le nom du fichier dans lequel la représentation
-    JSON sera enregistrée.
+    Args:
+        my_obj: L'objet à sérialiser et à écrire dans le fichier.
+        filename: Le nom du fichier dans lequel la représentation JSON sera enregistrée.
     """
-    with open(filename, 'w', encoding='utf-8' ) as file:
+    with open(filename, 'w', encoding='utf-8') as file:
         json.dump(my_obj, file)
