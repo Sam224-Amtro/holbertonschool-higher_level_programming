a595f130d8e3432671037356ca50a503bf92aef0
Samamtro <fofanasanoussy545@gmail.com>
2025-09-29 11:44:21 +0200

    the file to 2-append_write.

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define function Write file"""
+
+def append_write(filename="", text=""):
+    """
+    Ajoute du texte à la fin d'un fichier et retourne le nombre
+    de caractères ajoutés.
+    """
+    # Ouvre le fichier en mode ajout ("a"), UTF-8
+    with open(filename, "a", encoding="utf-8") as file:
+        # Écrit le texte et retourne le nombre de caractères ajoutés
+        return file.write(text)
