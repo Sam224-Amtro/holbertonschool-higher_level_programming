6f19c483a05159e6322f1ff9b93280f2dacddf1c
Samamtro <fofanasanoussy545@gmail.com>
2025-09-29 10:37:16 +0200

    the file to 0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,5 +3,5 @@
 
 def read_file(filename=""):
     """Reads a text file (UTF-8) and prints its content to stdout"""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, mode="r", encoding="utf-8") as file:
+        print(file.read(), end="")
