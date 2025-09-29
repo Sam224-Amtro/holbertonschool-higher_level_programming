426b681f97c345840517620bdde8df53edf7b6a5
Samamtro <fofanasanoussy545@gmail.com>
2025-09-29 14:04:28 +0200

    the file to 5-save_to_json_file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -12,7 +12,7 @@ def save_to_json_file(my_obj, filename):
         filename: Le nom du fichier dans lequel la représentation
         JSON sera enregistrée.
     """
-    
+
     # Ouvre le fichier en écriture et ferme automatiquement
     # le fichier à la fin.
     with open(filename, 'w', encoding='utf-8') as file:
