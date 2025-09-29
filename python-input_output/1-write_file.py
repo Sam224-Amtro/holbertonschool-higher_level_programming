1c6024ec0962ee2e4d78618ba9a1e9d812377362
Samamtro <fofanasanoussy545@gmail.com>
2025-09-29 11:25:58 +0200

    the file to 1-write_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,9 +7,5 @@ def write_file(filename="", text=""):
     Écrit une chaîne de caractères dans un fichier texte (UTF-8)
     et retourne le nombre de caractères écrits.
     """
-    try:
-        with open(filename, "w", encoding="utf-8") as file:
+    with open(filename, "w", encoding="utf-8") as file:
             return file.write(text)
-    except Exception as e:
-        print(f"Erreur lors de l'écriture du fichier '{filename}': {e}")
-        return 0
