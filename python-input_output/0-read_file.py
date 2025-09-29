ea3e915a4cc4e4f8f75a85123d9f88b5342825e2
Samamtro <fofanasanoussy545@gmail.com>
2025-09-29 10:30:35 +0200

    file to 0-read_file.

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,15 +9,5 @@ def read_file(filename=""):
     Args:
         filename (str): Nom du fichier à lire. Par défaut, une chaîne vide.
     """
-
-    # Le mot-clé "with" garantit que le fichier sera fermé automatiquement,
-    # même en cas d'erreur (bonne pratique pour la gestion des fichiers).
-    with open(filename, encoding="utf-8") as file:
-
-        # La méthode read() lit l’intégralité du contenu du
-        # fichier en une seule fois.
-        # Ici, on l’affiche directement avec print().
-        # Le paramètre end="" empêche print d’ajouter un saut
-        # de ligne supplémentaire,
-        # car le fichier contient peut-être déjà des retours à la ligne.
-        print(file.read(), end="")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
