07654bf2b1ea6a244d62953c0e723588a5cf9756
Samamtro <fofanasanoussy545@gmail.com>
2025-09-30 12:40:45 +0200

    the file to 9-student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class pour Student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initialisation de student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self):
+            """
+            Retourne la description d’un objet sous forme de dictionnaire.
+            """
+            return self.__dict__
