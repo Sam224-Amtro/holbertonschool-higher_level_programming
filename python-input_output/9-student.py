11a298c0929340b391770c28bd56abbf35987d81
Samamtro <fofanasanoussy545@gmail.com>
2025-09-30 12:58:59 +0200

    th file to 9-student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Class pour Student"""
+"""Class Student"""
 
 class Student:
     def __init__(self, first_name, last_name, age):
-        """Initialiser l'etudiant"""
+        """Initialize the studen"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
 
     def to_json(self):
-        """
-        Retourne un dictionnaire contenant les informations de l'étudiant
-        """
+        """Return the dictionary description"""
         return self.__dict__
