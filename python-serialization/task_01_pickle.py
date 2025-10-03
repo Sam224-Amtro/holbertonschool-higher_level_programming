cc7af865f8dc672770b38bcb87283ed6d4dcc250
Samamtro <fofanasanoussy545@gmail.com>
2025-10-03 11:01:45 +0200

    the file to task_01_pickle

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-"""Pickle Serialization"""
-
-
+'''Pickle Serialization'''
 import pickle
 
-class customObjet:
-    def __init__(self, name, age, is_student):
-        """
-        Initialisez le CustomObject avec les attributs nom, âge et est_étudiant.
-        """
+
+class CustomObject:
+    """Class Custom Object"""
+    def __init__(self, name: str, age: int, is_student: bool):
+        """Initialise the atribute of the object"""
         self.name = name
         self.age = age
         self.is_student = is_student
+
     def display(self):
         print(
             f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
-    def serialize(self, flamme: str):
-        """Serialize this objet"""
+
+    def serialize(self, filename: str):
+        """Serialize this object"""
         try:
-            with open(flamme, "wb") as file:
+            with open(filename, 'wb') as file:
                 pickle.dump(self, file)
         except Exception as e:
             print(f"Serialization Error: {e}")
+
     @classmethod
     def deserialize(cls, filename: str):
-        """Deserialize this objet"""
+        """Deserialize this object"""
         try:
-            with open(filename, "rb") as file:
+            with open(filename, 'rb') as file:
                 return pickle.load(file)
         except (FileNotFoundError, pickle.PickleError, EOFError) as e:
             print(f"Deserialization Error: {e}")
