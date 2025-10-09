3d867b57d1822ea2f1b520be8be77316b31bb82c
Samamtro <fofanasanoussy545@gmail.com>
2025-10-09 09:42:54 +0200

    the file to task_03_http_server.

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Defines the SimpleHTTPRequestHandler class for a basic API server."""
+
+
+import json
+import http.server
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Bonjour, ceci est une API simple !")
+
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "Alice", "age": 25, "city": "Paris"}
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            info = {"version": "1.0", "description":
+                    "Une API simple construite avec http.server"}
+            self.wfile.write(json.dumps(info).encode('utf-8'))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint non trouve")
+
+if __name__ == "__main__":
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print("Serveur démarré sur le port 8000...")
+    httpd.serve_forever()
