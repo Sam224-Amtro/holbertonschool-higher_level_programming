ef1c97b15c504f603366458ffb29271d64f4ef46
Samamtro <fofanasanoussy545@gmail.com>
2025-10-25 16:06:10 +0200

    the file to my 0-select_states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # connexion a la base de données
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    #Création d'un curseur
+    cur = db.cursor()
+
+    # Exécution de la requête SQL
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Récupération et affichage des résultates
+    for row in cur.fetchall():
+        print(row)
+
+    # Fermeture du curseur et de la connexion
+    cur.close()
+    db.close()
