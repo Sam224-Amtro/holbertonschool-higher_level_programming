653c2e8137e47888507f4904fc09b87a13fd8234
Samamtro <fofanasanoussy545@gmail.com>
2025-10-26 17:30:18 +0100

    the file to model_state_update_id_2

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Relie la classe State à la table dans la base de données et met à jour un enregistrement
+"""Start link class to table in database
 """
 from sys import argv
 from model_state import Base, State
@@ -8,26 +7,26 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    # Connexion à la base MySQL (avec user, mot de passe et nom de base en arguments)
+    # Création d'une instance de moteur SQLAlchemy
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True
+        pool_pre_ping=True,
     )
 
-    # Crée la table si elle n’existe pas
+    # Création des tables dans la base de données
     Base.metadata.create_all(engine)
 
-    # Ouvre une session avec la base
+    # Ouverture d'une session avec la base de données
     session = Session(engine)
 
-    # Cherche l’état avec l’id = 2
+    # Recherche du premier état dont l'id est 2
     state = session.query(State).filter(State.id.like(2)).first()
 
-    # Change son nom en "New Mexico"
+    # Modification du nom de l'état trouvé en 'New Mexico'
     state.name = 'New Mexico'
 
-    # Enregistre la modification
+    # Engagement de la session pour enregistrer le nouvel état
     session.commit()
 
-    # Ferme la session
+    # Fermeture de la session après la fin de l'exécution
     session.close()
