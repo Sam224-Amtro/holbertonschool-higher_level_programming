42c4a9143bfa7a568b89f8b15d76468939ad56fb
Samamtro <fofanasanoussy545@gmail.com>
2025-10-26 16:40:59 +0100

    the file to model_state_insert

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Adds the State object “Louisiana” to the database hbtn_0e_6_usa.
+Ajoute l'objet State "Louisiana" à la base de données hbtn_0e_6_usa.
 """
 
 from sys import argv
@@ -10,24 +10,24 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    # Create the engine and connect to the database
+    # Crée le moteur et se connecte à la base de données
     engine = create_engine(
         f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}",
         pool_pre_ping=True
     )
     Base.metadata.create_all(engine)
 
-    # Create a configured "Session" class
+    # Création d'une classe « Session » configurée
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Create a new State object and add it to the database
+    # Crée un nouvel objet State et l'ajoute à la base de données
     new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
 
-    # Print the new state's id
+    # Affiche l'id du nouvel état
     print(new_state.id)
 
-    # Close the session
+    # Ferme la session
     session.close()
