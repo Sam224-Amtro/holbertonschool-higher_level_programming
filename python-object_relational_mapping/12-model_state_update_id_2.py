15525d82882e34673c661ebcbd13ab17167e0a4b
Samamtro <fofanasanoussy545@gmail.com>
2025-10-26 17:23:22 +0100

    the file to model_state_update_id_2

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,22 +10,16 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    # Crée le moteur et se connecte à la base de données MySQL
     engine = create_engine(
         f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}",
         pool_pre_ping=True
     )
     Base.metadata.create_all(engine)
 
-    # Création d'une session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Recherche de l'objet State avec id = 2 et modification de son nom
     etat_a_modifier = session.query(State).filter_by(id=2).first()
     if etat_a_modifier:
         etat_a_modifier.name = "New Mexico"
         session.commit()
-
-    # Fermeture de la session
-    session.close()
