1d053a15f81697f04f7fc3681549eb6ac005d0ab
Samamtro <fofanasanoussy545@gmail.com>
2025-11-10 15:24:10 +0100

    the file to my arguments

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-// process.argv[0] -> node executable path
-// process.argv[1] -> script file path
-// process.argv[2] -> first argument passed
 
-const arg = process.argv[2]; // Safe: no .length used
+const arg = process.argv[2];
 
 if (arg === undefined) {
   console.log('No argument');
