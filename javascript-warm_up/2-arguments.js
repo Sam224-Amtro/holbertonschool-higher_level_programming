bfb06b9af74e9dd2e6429ae8feb26144edc2dd0d
Samamtro <fofanasanoussy545@gmail.com>
2025-11-10 11:52:55 +0100

    the file to my arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-
-// Récupère les arguments après "node script.js"
 const args = process.argv.slice(2);
 
-// Déterminer le message en fonction du nombre d'arguments
 if (args.length === 0) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (args.length === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
-
