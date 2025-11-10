86b62a77a3a465eb9c24f2b42285014e3c3a5a24
Samamtro <fofanasanoussy545@gmail.com>
2025-11-10 18:48:27 +0100

    the file to second_biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,10 @@
 #!/usr/bin/node
-// Script pour calculer la factorielle d'un entier non négatif
 
-// Fonction factorielle récursive
-function factorial (n) {
-  // Si n n'est pas un entier ou est négatif, retourne 1
-  if (!Number.isInteger(n) || n < 0) return 1;
+const list = process.argv.slice(2).map(Number);
 
-  // Cas de base : 0! et 1! valent 1
-  if (n <= 1) return 1;
-
-  // Cas récursif
-  return n * factorial(n - 1);
+if (list.length <= 1) {
+  console.log(0);
+} else {
+  const sorted = list.sort((a, b) => b - a);
+  console.log(sorted[1]);
 }
-
-// Récupère le premier argument passé en ligne de commande et
-// le convertit en entier
-const num = parseInt(process.argv[2], 10);
-
-// Calcule et affiche la factorielle
-console.log(factorial(num));
