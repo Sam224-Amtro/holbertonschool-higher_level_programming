f4555d28bb7f77d96bd427401125f4cc3ac99d40
Samamtro <fofanasanoussy545@gmail.com>
2025-11-10 16:23:27 +0100

    the file to multi_languages_loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Tableau de chaînes à imprimer
+const messages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Parcourez le tableau et imprimez chaque message
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
