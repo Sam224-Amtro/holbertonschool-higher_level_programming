3af7f7145d9d78f1452c61dd8210f03f060a8b79
Samamtro <fofanasanoussy545@gmail.com>
2025-11-10 11:13:04 +0100

    the file to my multi_languages

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
+// Definition des constactes de chaque ligne
 const line1 = 'C is fun';
 const line2 = 'Python is cool';
 const line3 = 'JavaScript is amazing';
 
+// Imprimez chaque ligne
 console.log(line1);
 console.log(line2);
 console.log(line3);
