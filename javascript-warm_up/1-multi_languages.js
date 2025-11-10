bf9d9fb6ef98760287b86f7cb744a589e54387f2
Samamtro <fofanasanoussy545@gmail.com>
2025-11-10 11:04:41 +0100

    the file to my multi_languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+// Definition des constantes pour chaque ligne
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+
+//Imprimer chaque ligne
+console.log(line1);
+console.log(line2);
+console.log(line3);
+
+
+
