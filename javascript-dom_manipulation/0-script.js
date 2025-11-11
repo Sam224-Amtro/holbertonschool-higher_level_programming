40d1c3cab14c8b4532661b7055cde59f2fca7971
Samamtro <fofanasanoussy545@gmail.com>
2025-11-11 12:14:29 +0100

    the file to my script

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 
-// Sélectionne la balise <header>
-const header = document.querySelector('header');
-
-// Change la couleur du texte en rouge
-header.style.color = '#FF0000';
+// Sélectionne la balise <header> et change la couleur du texte en rouge
+document.querySelector('header').style.color = '#FF0000';
