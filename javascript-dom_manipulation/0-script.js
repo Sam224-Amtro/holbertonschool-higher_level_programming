b2dca6c0f5de8bc6fbae0f899f7319b2a3edf4c6
Samamtro <fofanasanoussy545@gmail.com>
2025-11-11 11:53:13 +0100

    this is my file main, script and README

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Sélectionne la balise <header>
+const header = document.querySelector('header');
+
+// Change la couleur du texte en rouge
+header.style.color = '#FF0000';
---
+++ javascript-dom_manipulation/README.md
@@ -0,0 +1,1 @@
+Welcome to the projet javascript-dom_manipulation
